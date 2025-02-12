7a41666876adbfae8e9b361e543a1dcd073c3b29
Talha_Durmaz <144232044+talhadrz@users.noreply.github.com>
2025-02-12 15:25:22 +0300

    Dosya güncellendi

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basit bir karşılama mesajı
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Web sitesine hoş geldiniz! Talha'nın projelerine göz atmayı unutmayın.");
+  });
+  
